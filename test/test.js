3aaa04322056d65c9af40569165fca857ebedd64
Cas <c.vanderlaan@livetech-systems.com>
2019-02-06 09:57:42 +0100

    ddd

---
+++ test/test.js
@@ -0,0 +1,10 @@
+(() => {
+  'use strict';
+  const assert = require('assert');
+  const myHello = require('../src/app');
+  describe('Test', function() {
+    it('should pass', function() {
+      assert.equal(myHello.dispatch.start(), 'Done');
+    });
+  });
+})();
